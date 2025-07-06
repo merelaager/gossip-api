f4b7decc5d235f9dd92bfc4504aa4f9859e58fc9
Taka <71265675+takakv@users.noreply.github.com>
2025-07-06 04:30:01 +0300

    Fetch posts

--- src/routes/posts/index.ts
+++ src/routes/posts/index.ts
@@ -1,9 +1,40 @@
 import { FastifyPluginAsyncTypebox } from "@fastify/type-provider-typebox";
-import { StatusCodes } from "http-status-codes";
+import prisma from "../../utils/prisma";
+import { createSuccessResponse } from "../../utils/jsend";
 
 const postsRoute: FastifyPluginAsyncTypebox = async (fastify) => {
-  fastify.get("/", (request, response) => {
-    response.status(StatusCodes.OK).send({ message: "Server is up!" });
+  fastify.get("/", async (request, response) => {
+    const { user } = request.session;
+
+    const userData = await prisma.user.findUnique({
+      where: { id: user.userId },
+      select: { shift: true },
+    });
+
+    if (!userData) {
+      return response.send(
+        createSuccessResponse({ posts: [], currentPage: 1, totalPages: 1 }),
+      );
+    }
+
+    const postsPerPage = 15;
+    const pageNumber = 1;
+
+    const publishedPostCount = await prisma.post.count({
+      where: { shift: userData.shift, published: true, hidden: false },
+    });
+    const totalPages = Math.ceil(publishedPostCount / postsPerPage);
+
+    const posts = await prisma.post.findMany({
+      where: { shift: userData.shift, published: true, hidden: false },
+      orderBy: { createdAt: "desc" },
+      skip: postsPerPage * (pageNumber - 1),
+      take: postsPerPage,
+    });
+
+    return response.send(
+      createSuccessResponse({ posts, currentPage: pageNumber, totalPages }),
+    );
   });
 };
 
