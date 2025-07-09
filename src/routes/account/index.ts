4232d63ba2fa7836b49feb121acd372166d8e1e5
Taka <71265675+takakv@users.noreply.github.com>
2025-07-10 01:42:47 +0300

    Implement password change

--- src/routes/account/index.ts
+++ src/routes/account/index.ts
@@ -1,11 +1,15 @@
 import { Type } from "@sinclair/typebox";
-import { FastifyPluginAsyncTypebox } from "@fastify/type-provider-typebox";
+import type { FastifyPluginAsyncTypebox } from "@fastify/type-provider-typebox";
 import { StatusCodes } from "http-status-codes";
+import * as argon2 from "argon2";
 
 import prisma from "../../utils/prisma.js";
-import { createSuccessResponse } from "../../utils/jsend.js";
+import {
+  createFailResponse,
+  createSuccessResponse,
+} from "../../utils/jsend.js";
 
-import { SuccessResponse } from "../../schemas/jsend.js";
+import { FailResponse, SuccessResponse } from "../../schemas/jsend.js";
 
 const postsRoute: FastifyPluginAsyncTypebox = async (fastify) => {
   fastify.get(
@@ -42,6 +46,43 @@ const postsRoute: FastifyPluginAsyncTypebox = async (fastify) => {
           role: user.role,
         }),
       );
+    },
+  );
+  fastify.post(
+    "/change-password",
+    {
+      schema: {
+        body: Type.Object({
+          newPassword: Type.String(),
+        }),
+        response: {
+          [StatusCodes.BAD_REQUEST]: FailResponse(
+            Type.Object({
+              message: Type.String(),
+            }),
+          ),
+        },
+      },
+    },
+    async (request, reply) => {
+      const { userId } = request.session.user;
+      const { newPassword } = request.body;
+
+      if (newPassword.length < 8) {
+        return reply.status(StatusCodes.BAD_REQUEST).send(
+          createFailResponse({
+            message: "Salasõna on liiga lühike.",
+          }),
+        );
+      }
+
+      const passwordHash = await argon2.hash(newPassword);
+      await prisma.user.update({
+        where: { id: userId },
+        data: { password: passwordHash },
+      });
+
+      return reply.status(StatusCodes.NO_CONTENT).send();
     },
   );
 };
